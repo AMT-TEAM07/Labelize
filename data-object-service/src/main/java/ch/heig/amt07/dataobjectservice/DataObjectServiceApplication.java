e70a1caca6e27854c590a9d9771b2f7446d2f1d8
Lazzzer <dev@lazarpavicevic.ch>
2022-11-29 15:46:54 +0100

    chore: push files in data-object-service

---
+++ data-object-service/src/main/java/ch/heig/amt07/dataobjectservice/DataObjectServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.heig.amt07.dataobjectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataObjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataObjectServiceApplication.class, args);
+    }
+
+}
