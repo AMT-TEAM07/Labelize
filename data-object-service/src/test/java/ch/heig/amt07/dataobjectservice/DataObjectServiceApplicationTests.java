d738effe34fdd3a6fc28fd0d4cdc247094e4924a
Lazzzer <dev@lazarpavicevic.ch>
2022-11-29 15:34:50 +0100

    chore: add project for data object service

---
+++ data-object-service/src/test/java/ch/heig/amt07/dataobjectservice/DataObjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heig.amt07.dataobjectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataObjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
