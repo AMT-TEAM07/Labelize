92ca3f6fcd626413d0c4b19d8aa6930f1c5e0025
Jonathan <chil.tfm@gmail.com>
2022-11-29 15:35:43 +0100

    chore: create projet for label detection service

---
+++ label-detector-service/src/main/java/ch/heig/amt07/labeldetectorservice/LabelDetectorServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.heig.amt07.labeldetectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelDetectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelDetectorServiceApplication.class, args);
+	}
+
+}
---
+++ label-detector-service/src/test/java/ch/heig/amt07/labeldetectorservice/LabelDetectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heig.amt07.labeldetectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelDetectorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
