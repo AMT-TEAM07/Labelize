63edf6445b801417a6e06f4b3c24a04765a9defd
Jonathan <chil.tfm@gmail.com>
2022-11-29 17:28:21 +0100

    feat(dataObject): add tests and code from previous repo

---
+++ data-object-service/src/main/java/ch/heig/amt07/dataobjectservice/service/AwsDataObjectHelper.java
@@ -0,0 +1,121 @@
+package ch.heig.amt07.dataobjectservice.service;
+
+import ch.heig.amt07.dataobjectservice.utils.AwsConfigProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AwsDataObjectHelper implements DataObjectHelper {
+
+    private final AwsConfigProvider configProvider;
+    private static final Logger LOG = Logger.getLogger(AwsDataObjectHelper.class.getName());
+    private final S3Client s3;
+    private final String rootObjectName;
+
+    public AwsDataObjectHelper(AwsConfigProvider configProvider, String rootObjectName) {
+        this.configProvider = configProvider;
+        this.rootObjectName = rootObjectName;
+        s3 = S3Client.builder()
+                .region(configProvider.getRegion())
+                .credentialsProvider(configProvider.getCredentialsProvider())
+                .build();
+    }
+
+    public boolean existsRootObject(String rootObjectName) {
+        HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
+                .bucket(rootObjectName)
+                .build();
+        try {
+            s3.headBucket(headBucketRequest);
+            return true;
+        } catch (NoSuchBucketException e) {
+            LOG.log(Level.INFO, "{0}", e.getMessage());
+            return false;
+        }
+    }
+
+    public void createRootObject(String rootObjectName) {
+        if (!existsRootObject(rootObjectName)) {
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                    .bucket(rootObjectName)
+                    .build();
+            s3.createBucket(createBucketRequest);
+        }
+    }
+
+    public void removeRootObject(String rootObjectName) {
+        if (existsRootObject(rootObjectName)) {
+            DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder()
+                    .bucket(rootObjectName)
+                    .build();
+            s3.deleteBucket(deleteBucketRequest);
+        }
+    }
+
+    public void createObject(String objectName, Path filePath) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(rootObjectName)
+                .key(objectName)
+                .build();
+        s3.putObject(objectRequest, RequestBody.fromFile(filePath));
+    }
+
+    public boolean existsObject(String objectName) {
+        HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                .bucket(rootObjectName)
+                .key(objectName)
+                .build();
+        try {
+            s3.headObject(headObjectRequest);
+            return true;
+        } catch (NoSuchKeyException e) {
+            LOG.log(Level.INFO, "{0}", e.getMessage());
+            return false;
+        }
+    }
+
+    public void removeObject(String objectName) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(rootObjectName)
+                .key(objectName)
+                .build();
+        s3.deleteObject(deleteObjectRequest);
+    }
+
+    public boolean downloadObject(String objectUrl, Path downloadedImagePath) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(rootObjectName)
+                .key(objectUrl)
+                .build();
+        try {
+            s3.getObject(getObjectRequest, downloadedImagePath);
+            return true;
+        } catch (NoSuchKeyException e) {
+            LOG.log(Level.INFO, "{0}", e.getMessage());
+            return false;
+        }
+    }
+
+    public String getPresignedUrl(String objectName) {
+        try (S3Presigner presigner = S3Presigner.builder()
+                .credentialsProvider(configProvider.getCredentialsProvider())
+                .region(configProvider.getRegion())
+                .build()) {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(rootObjectName)
+                    .key(objectName)
+                    .build();
+            GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(java.time.Duration.ofMinutes(2))
+                    .getObjectRequest(getObjectRequest)
+                    .build();
+            return presigner.presignGetObject(getObjectPresignRequest).url().toString();
+        }
+    }
+}
--- data-object-service/src/test/java/ch/heig/amt07/dataobjectservice/DataObjectServiceApplicationTests.java
+++ data-object-service/src/test/java/ch/heig/amt07/dataobjectservice/DataObjectServiceApplicationTests.java
@@ -1,14 +1,183 @@
 package ch.heig.amt07.dataobjectservice;
 
+import com.sun.tools.javac.Main;
+import io.github.cdimascio.dotenv.Dotenv;
+import ch.heig.amt07.dataobjectservice.service.AwsDataObjectHelper;
+import ch.heig.amt07.dataobjectservice.utils.AwsConfigProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DataObjectServiceApplicationTests {
 
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    private AwsDataObjectHelper bucketManager;
+    private String bucketName;
+    private Path testImagePath;
+    private Path downloadedImagePath;
+    private String objectName;
+
+    @BeforeEach
+    public void setup() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .systemProperties()
+                .load();
+
+        bucketName = dotenv.get("TEST_AWS_BUCKET");
+        objectName = "test-image.png";
+        testImagePath = Paths.get("src", "test", "resources", objectName);
+        downloadedImagePath = Paths.get("src", "test", "resources", "downloaded-" + objectName);
+
+        var configProvider = new AwsConfigProvider("TEST_AWS_ACCESS_KEY_ID", "TEST_AWS_SECRET_ACCESS_KEY", "TEST_AWS_DEFAULT_REGION");
+        bucketManager = new AwsDataObjectHelper(configProvider, bucketName);
+    }
+
     @Test
-    void contextLoads() {
-        assert(true);
+    void canCreateObjectInExistingRootObject() {
+        //given
+        assertTrue(bucketManager.existsRootObject(bucketName));
+        assertFalse(bucketManager.existsObject(objectName));
+
+        //when
+        bucketManager.createObject(objectName, testImagePath);
+
+        //then
+        assertTrue(bucketManager.existsObject(objectName));
+    }
+
+    @Test
+    void canConfirmRootObjectExists() {
+        //given
+        String existingBucket = bucketName;
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.existsRootObject(existingBucket);
+
+        //then
+        assertTrue(actualResult);
+    }
+
+    @Test
+    void canConfirmRootObjectDoesNotExist() {
+        //given
+        String notExistingBucket = "notExistingBucket-" + bucketName;
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.existsRootObject(notExistingBucket);
+
+        //then
+        assertFalse(actualResult);
+    }
+
+    @Test
+    void canConfirmObjectExists() {
+        //given
+        bucketManager.createObject(objectName, testImagePath);
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.existsObject(objectName);
+
+        //then
+        assertTrue(actualResult);
+    }
+
+    @Test
+    void canConfirmObjectDoesNotExist() {
+        //given
+        String notExistingFileName = "notExistingFile.jpg";
+        assertTrue(bucketManager.existsRootObject(bucketName));
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.existsObject(notExistingFileName);
+
+        //then
+        assertFalse(actualResult);
+    }
+
+    @Test
+    void canRemoveObjectFromNotEmptyRootObject() {
+        //given
+        assertTrue(bucketManager.existsRootObject(bucketName));
+        bucketManager.createObject(objectName, testImagePath);
+        assertTrue(bucketManager.existsObject(objectName));
+
+        //when
+        bucketManager.removeObject(objectName);
+
+        //then
+        assertFalse(bucketManager.existsObject(objectName));
+    }
+
+    @Test
+    void canDownloadExistingObject() {
+        //given
+        bucketManager.createObject(objectName, testImagePath);
+        assertTrue(bucketManager.existsObject(objectName));
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.downloadObject(objectName, downloadedImagePath);
+
+        //then
+        assertTrue(actualResult);
+        File file = new File(downloadedImagePath.toUri());
+        assertTrue(file.exists());
+        assertEquals(file.length(), testImagePath.toFile().length());
+    }
+
+    @Test
+    void returnFalseWhenDownloadingNotExistingObject() {
+        //given
+        assertFalse(bucketManager.existsObject(objectName));
+        boolean actualResult;
+
+        //when
+        actualResult = bucketManager.downloadObject(objectName, downloadedImagePath);
+
+        //then
+        assertFalse(actualResult);
+        File file = new File(downloadedImagePath.toUri());
+        assertFalse(file.exists());
+    }
+
+    @Test
+    void canGeneratePresignedPublicUrlForGivenObjectName() {
+        //given
+        bucketManager.createObject(objectName, testImagePath);
+        assertTrue(bucketManager.existsObject(objectName));
+        String actualResult;
+
+        //when
+        actualResult = bucketManager.getPresignedUrl(objectName);
+
+        //then
+        assertNotNull(actualResult);
+    }
+
+    @AfterEach
+    void tearDown() {
+        File file = new File(downloadedImagePath.toUri());
+        if (file.exists()) {
+            LOG.log(Level.INFO, "{0}", "Deleting file => " + file.delete());
+        }
+        if (bucketManager.existsObject(objectName)) {
+            bucketManager.removeObject(objectName);
+        }
     }
 
 }
