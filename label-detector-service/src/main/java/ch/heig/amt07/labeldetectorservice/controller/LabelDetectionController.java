605a0f3a6e381e779b9376a13dc8d05d35398b89
Jonathan <chil.tfm@gmail.com>
2022-12-13 15:00:06 +0100

    feat(label): add hypermedia to response

--- label-detector-service/src/main/java/ch/heig/amt07/labeldetectorservice/controller/LabelDetectionController.java
+++ label-detector-service/src/main/java/ch/heig/amt07/labeldetectorservice/controller/LabelDetectionController.java
@@ -2,11 +2,16 @@ package ch.heig.amt07.labeldetectorservice.controller;
 
 import ch.heig.amt07.labeldetectorservice.service.LabelWrapper;
 import ch.heig.amt07.labeldetectorservice.utils.AnalyzeParams;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import ch.heig.amt07.labeldetectorservice.service.*;
 
 import java.io.IOException;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 class LabelDetectionController{
@@ -17,12 +22,16 @@ class LabelDetectionController{
     }
 
     @PostMapping("/analyze/url")
-    List<LabelWrapper> analyzeFromUrl(@RequestBody AnalyzeParams params) throws IOException {
-        return labelDetector.execute(params.image(), params.maxLabels(), params.minConfidence());
+    CollectionModel<EntityModel<LabelWrapper>> analyzeFromUrl(@RequestBody AnalyzeParams params) throws IOException {
+        List<EntityModel<LabelWrapper>> labels = labelDetector.execute(params.image(), params.maxLabels(), params.minConfidence())
+                .stream().map(EntityModel::of).toList();
+        return CollectionModel.of(labels, linkTo(methodOn(LabelDetectionController.class).analyzeFromB64(params)).withRel("Base64"));
     }
 
     @PostMapping("/analyze/b64")
-    List<LabelWrapper> analyzeFromB64(@RequestBody AnalyzeParams params) throws IOException {
-        return labelDetector.executeB64(params.image(), params.maxLabels(), params.minConfidence());
+    CollectionModel<EntityModel<LabelWrapper>>  analyzeFromB64(@RequestBody AnalyzeParams params) throws IOException {
+        List<EntityModel<LabelWrapper>> labels = labelDetector.executeB64(params.image(), params.maxLabels(), params.minConfidence())
+                .stream().map(EntityModel::of).toList();
+        return CollectionModel.of(labels, linkTo(methodOn(LabelDetectionController.class).analyzeFromUrl(params)).withRel("fromUrl"));
     }
 }
