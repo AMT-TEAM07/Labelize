2faeadf5f1ca5c6ee036691254eed07e94e8d2e6
Lazzzer <dev@lazarpavicevic.ch>
2022-12-17 14:20:26 +0100

    test: add scenarios

--- cli-client/src/test/java/MainTest.java
+++ cli-client/src/test/java/MainTest.java
@@ -1,8 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
+
+    @BeforeEach
+    void setup() {
+        System.out.println("setUp");
+    }
+
+    @Disabled("to not spam bucket manipulation")
     @Test
-    void tempTestForPipeline() {
+    void Scenario_One_Nothing_Exists() {
+        //given
+
+        //when
+
+        //then
         assert(true);
+    }
+
+    @Disabled("Not implemented yet")
+    @Test
+    void Scenario_Two_Only_RootObject_Exists() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Disabled("Not implemented yet")
+    @Test
+    void Scenario_Three_Everything_Exists() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("tearDown");
     }
 }
